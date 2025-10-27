3571e46ef97c420a525964be52b67fdc3aba1df2
Nazar <nazar.webpost@gmail.com>
2025-10-27 14:39:01 +0100

    Refactor Home component to improve UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { onLog, onStep, start, stop } from "../native/StepBleClient";
 
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const off1 = onStep((n) => setSteps(n));
-    const off2 = onLog((m) => setLogs((l) => [m, ...l].slice(0, 8)));
+    const off2 = onLog((m) => setLogs((l) => [m, ...l].slice(0, 5)));
     return () => {
       off1();
       off2();
@@ -22,19 +22,41 @@ export default function Home() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Steps</Text>
-      <Text style={styles.steps}>{steps}</Text>
-      <View style={styles.buttons}>
-        <Button title="Start BLE" onPress={() => start(SERVICE, CHAR)} />
-        <Button title="Stop" onPress={() => stop()} />
+      <View style={styles.body}>
+        <Text style={styles.title}>STEPS</Text>
+        <View style={styles.stepsContainer}>
+          <Text style={styles.steps}>{steps}</Text>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              start(SERVICE, CHAR);
+            }}
+          >
+            <Text style={styles.buttonText}>START</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              stop();
+            }}
+          >
+            <Text style={styles.buttonText}>STOP</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={styles.logsContainer}>
-        <Text style={styles.logsTitle}>Logs:</Text>
-        {logs.map((l, i) => (
-          <Text key={i} style={styles.logs}>
-            {l}
-          </Text>
-        ))}
+        {logs.length > 0 && (
+          <>
+            <Text style={styles.logsTitle}>Logs:</Text>
+            {logs.map((l, i) => (
+              <Text key={i} style={styles.logs}>
+                {l}
+              </Text>
+            ))}
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -48,12 +70,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  body: {
+    flex: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   title: {
-    fontSize: 42,
-    fontWeight: "400",
+    fontSize: 72,
+    fontWeight: "bold",
+  },
+  stepsContainer: {
+    width: 180,
+    height: 180,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 10,
+    borderColor: "red",
+    borderRadius: 100,
+    marginVertical: 24,
   },
   steps: {
-    fontSize: 92,
+    fontSize: 72,
     fontWeight: "bold",
   },
   buttons: {
@@ -66,7 +104,22 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   logsContainer: {
+    flex: 2,
     marginTop: 24,
     width: "100%",
+  },
+  button: {
+    width: 100,
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
